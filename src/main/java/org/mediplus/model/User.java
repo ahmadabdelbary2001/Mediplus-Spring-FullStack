cfa21b6d9143b242eadee1f836605088b8e22542
ahmadabdelbary2001 <ahmadabdelbary2001@gmail.com>
2025-05-18 08:39:36 +0300

    Implement home controller and user model classes

--- src/main/java/org/mediplus/model/User.java
+++ src/main/java/org/mediplus/model/User.java
@@ -1,4 +1,24 @@
 package org.mediplus.model;
 
-public class User {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class User {
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+    // Constructors
+    public User() {}
+    public User(Long id, String username, String email, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
 }
