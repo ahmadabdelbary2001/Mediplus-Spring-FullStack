d033f90e547a10873c9f844b7491dcfd9d58c1ca
ahmadabdelbary2001 <ahmadabdelbary2001@gmail.com>
2025-06-04 01:05:52 +0300

    Integrate UserResponseDTO & LoginDTO, adapt code

---
+++ src/main/java/org/mediplus/user/LoginDTO.java
@@ -0,0 +1,23 @@
+package org.mediplus.user;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginDTO {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    public LoginDTO() {}
+
+    public LoginDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
---
+++ src/main/java/org/mediplus/user/UserResponseDTO.java
@@ -0,0 +1,21 @@
+package org.mediplus.user;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResponseDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private String role;
+
+    public UserResponseDTO() {}
+
+    public UserResponseDTO(Long id, String username, String email, String role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.role = role;
+    }
+}
